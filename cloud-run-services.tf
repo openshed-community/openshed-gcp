7f25fa8e93d22be94be81f7688143709452d3c5e
Vincent Catros <vincent.catros@orange.com>
2023-05-02 17:53:11 +0200

    Initial commit

---
+++ cloud-run-services.tf
@@ -0,0 +1,36 @@
+resource "google_cloud_run_service" "openshed" {
+  name = var.gcp_service_name
+  location = var.gcp_location
+
+  template {
+    spec {
+      containers {
+        image = "begblev/openshed:latest"
+        env {
+          name = "ALLOWED_HOSTS"
+          value = var.django_allowed_hosts
+        }
+        env {
+          name = "DATABASE_URL"
+          value = var.django_database_url
+        }
+        env {
+          name = "SECRET_KEY"
+          value = var.django_secret_key
+        }
+        dynamic env {
+          for_each = var.django_debug ? [1] : []
+          content {
+            name = "DEBUG"
+            value = true
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent = 100
+    latest_revision = true
+  }
+}
